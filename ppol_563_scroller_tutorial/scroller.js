834f00dc61635b6e76f951d1c6dad00d00461d87
vegalla <70249519+vegalla@users.noreply.github.com>
2021-11-08 14:50:32 -0500

    Comment updates

--- ppol_563_scroller_tutorial/scroller.js
+++ ppol_563_scroller_tutorial/scroller.js
@@ -1,38 +1,37 @@
 /**
- * scroller
- * handles the details of figuring out which section
- * the user is currently scrolled on
+ * Scroller
+ * This function handles the identifies which section the user is currently scrolled on
  */
 
 function scroller(){
     let container = d3.select('body')
-    // event dispatcher
+    // Event dispatcher
     let dispatch = d3.dispatch('active', 'progress');
-    // d3 selection of text sections that will be scrolled through
+    // D3 selection of text sections that will be scrolled through
     let sections = d3.selectAll('.step')
-    // empty array that will hold the y coordinate of each section that will be scrolled through
+    // Empty array that will hold the y coordinate of each section that will be scrolled through
     let sectionPositions
    
     let currentIndex = -1
-    // initial y coordinate
+    // Initial y coordinate
     let containerStart = 0;
 
     /**
-     * scroll - constructor function.
-     * sets up scroller to monitor scrolling of text sections
+     * Scroll - constructor function.
+     * Sets up scroller to monitor scrolling of text sections
      */
     function scroll(){
-        // when window is scrolled, call position function
-        // when window is resized, call resize function
-            // the resize function is important for building a dynamic web page that adapts to screen resolution
+        // When window is scrolled, call position function
+        // When window is resized, call resize function
+            // The resize function is important for building a dynamic web page that adapts to screen resolution
         d3.select(window)
             .on('scroll.scroller', position)
             .on('resize.scroller', resize)
 
-        // manually call resize to initially set up scroller
+        // Manually call resize to initially set up scroller
         resize();
 
-        // 'hack' to get position to be called once for scroll position on load
+        // 'Hack' to get position to be called once for scroll position on load
         let timer = d3.timer(function() {
             position();
             timer.stop();
@@ -40,8 +39,8 @@ function scroller(){
     }
 
     /**
-     * resize - called initially and when page is resized.
-     * resets the sectionPositions
+     * Resize - called initially and when page is resized.
+     * Resets the sectionPositions
      */
     function resize(){
         sectionPositions = [];
@@ -58,8 +57,8 @@ function scroller(){
     }
 
     /**
-     * position - get current users position
-     * if user has scrolled to new section, dispatch active event with new section index
+     * Position - get current users position
+     * If user has scrolled to new section, dispatch active event with new section index
      */
     function position() {
         let pos = window.pageYOffset - 300 - containerStart;
@@ -78,9 +77,9 @@ function scroller(){
     }
 
     /**
-     * container - get/set the parent element of the sections
-     * useful for if the scrolling doesn't start at the very top of the page
-     * for example, when the page is refreshed but doesn't reset at the top of the page
+     * Container - get/set the parent element of the sections
+     * Useful for if the scrolling doesn't start at the very top of the page
+     * For example, when the page is refreshed but doesn't reset at the top of the page
      */
     scroll.container = function(value) {
         if (arguments.legth === 0){
--- ppol_563_scroller_tutorial/sections.js
+++ ppol_563_scroller_tutorial/sections.js
@@ -1,8 +1,9 @@
+// Visualization parameters
 const margin = {left: 170, top: 50, bottom: 50, right: 20}
 const width = 1000 - margin.left - margin.right
 const height = 950 - margin.top - margin.bottom
 
-    // append the svg object to the body of the page
+// Append the svg object to the body of the page, the 'vis' div
 var svg = d3.select("#vis")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -11,6 +12,7 @@ var svg = d3.select("#vis")
     .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
+// Read in DC Metro Graph data and perform the following function to draw it
 d3.json("data/dc_metro_graph.json", function(data) {
 
     // Initialize the links
@@ -30,13 +32,14 @@ d3.json("data/dc_metro_graph.json", function(data) {
         .attr("r", 5)
         .style("fill", "#69b3a2")
 
-    // Let's list the force we wanna apply on the network
+    // Force specification
+    // This produces the network layout from the initial draw of all nodes as a single circle in the corner.
     var simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
         .force("link", d3.forceLink()                               // This force provides links between nodes
-                .id(function(d) { return d.id; })                     // This provide  the id of a node
-                .links(data.links)                                    // and this the list of links
+                .id(function(d) { return d.id; })                   // This provides the id of a node
+                .links(data.links)                                  // and this the list of edges
         )
-        .force("charge", d3.forceManyBody().strength(-20))          // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+        .force("charge", d3.forceManyBody().strength(-20))          // This adds repulsion between nodes
         .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
         .on("end", ticked);
 
@@ -54,29 +57,19 @@ d3.json("data/dc_metro_graph.json", function(data) {
     }
 })
 
+// This specifies that scrolling occurs over the 'graphic' div that contains the text content on the left side.
 let scroll = scroller()
     .container(d3.select('#graphic'))
+
 scroll()
 
-let lastIndex, activeIndex = 0
-
 scroll.on('active', function(index){
+    // When index in inactive, opacity is reduced for the fade effect
     d3.selectAll('.step')
+        // This transition call specifies over how much time the fade effect should occur.
+        // It specifies a duration of 500ms
         .transition().duration(500)
         .style('opacity', function (d, i) {return i === index ? 1 : 0.1;});
-    
-    activeIndex = index
-    let sign = (activeIndex - lastIndex) < 0 ? -1 : 1; 
-    let scrolledSections = d3.range(lastIndex + sign, activeIndex + sign, sign);
-    scrolledSections.forEach(i => {
-        activationFunctions[i]();
-    })
-    lastIndex = activeIndex;
 
-})
-
-scroll.on('progress', function(index, progress){
-    if (index == 2 & progress > 0.7){
-
-    }
+    // Compared to Chow's version, I've stripped out code related to various activations of his visualization for now.
 })
